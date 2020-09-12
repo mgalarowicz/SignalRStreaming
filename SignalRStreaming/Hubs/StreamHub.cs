484de7308bd558d359c1571354c2009317bf880c
Marek Galarowicz <marek.galarowicz@enova.pl>
2020-09-12 11:01:49 +0200

    Add token

--- SignalRStreaming/Hubs/StreamHub.cs
+++ SignalRStreaming/Hubs/StreamHub.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 
@@ -9,21 +10,29 @@ namespace SignalRStreaming.Hubs
 {
     public class StreamHub : Hub
     {
-        public ChannelReader<int> DelayCounter(int delay)
+        public ChannelReader<int> DelayCounter(int delay, CancellationToken cancellationToken)
         {
             var channel = Channel.CreateUnbounded<int>();
-            _ = WriteItems(channel.Writer, 20, delay);
+            _ = WriteItems(channel.Writer, 20, delay, cancellationToken);
             return channel.Reader;
         }
-        private async Task WriteItems(ChannelWriter<int> writer, int count, int delay)
+        private async Task WriteItems(ChannelWriter<int> writer, int count, int delay, CancellationToken cancellationToken)
         {
+            try
+            {
             for (var i = 0; i < count; i++)
             {
+                cancellationToken.ThrowIfCancellationRequested();
                 //For every 5 items streamed, add twice the delay
                 if (i % 5 == 0)
                     delay = delay * 2;
                 await writer.WriteAsync(i);
                 await Task.Delay(delay);
+            }
+            }
+            catch (Exception ex)
+            {
+                writer.TryComplete(ex);
             }
             writer.TryComplete();
         }
